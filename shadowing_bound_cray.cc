8e29abb35b4fbff9a63a340ff8988b77f898f3dc
Holger Thies <info@holgerthies.com>
2015-02-12 16:58:29 +0100

    output of shadowing bound for different precisions and parameters

--- shadowing_bound_cray.cc
+++ shadowing_bound_cray.cc
@@ -8,6 +8,7 @@
 #include <boost/multiprecision/mpfr.hpp>  // Defines the Backend type that wraps MPFR
  #include <boost/multiprecision/cpp_bin_float.hpp> 
 #include "fixed_precision_interval.h"
+#include <boost/mpl/for_each.hpp>
 
 namespace mp = boost::multiprecision;    
 const unsigned int cray_double_precision = 96;
@@ -98,8 +99,14 @@ number<double_prec> shadowing_bound(const int N, const number<double_prec> a, co
 	fixed_precision_interval<double_prec> x = fixed_precision_interval<double_prec>(static_cast<number<double_prec>>(orbit[N]), static_cast<number<double_prec>>(orbit[N]));
 	number<double_prec> beta = 0; // max distance between the p_n and the shadow orbit
 	for(int i=N-1; i>=0; i--){
+		// if(i % 1000000 == 0){ 
+		// 	cout<<i<<endl;
+		// 	cout<<x.right<<" "<<a/4<<endl;
+		// }
 		bool left= (orbit[i] < number<double_prec>(0.5));
 		x = prev_interval(x, a, thickening_constant, error_constant, left);
+		if(x.right > a/4)
+			return -1;
 		beta = std::max(beta, x.max_dist(static_cast<number<double_prec>>(orbit[i])));
 	}
 	return beta;
@@ -108,17 +115,23 @@ number<double_prec> shadowing_bound(const int N, const number<double_prec> a, co
 // checks if a breakdown occurs when using the procedure to compute the  
 // pseudo orbit up to the Nth iterate. A breakdown point is a point that 
 // is bigger than a/4, since then the inverse function is not defined
-// bool has_breakdown(const int N){
-// 	vector<number<cray_float_precision>> orbit = pseudo_orbit<cray_float_precision>(N);
-// 	cray_interval x = cray_interval(static_cast<cray_double>(orbit[N]), static_cast<cray_double>(orbit[N]));
-// 	for(int i=N-1; i>=0; i--){
-// 		bool left= (orbit[i] < cray_double(0.5));
-// 		x = prev_interval(x, left);
-// 		if(x.right > a/4)
-// 			return true;
-// 	}
-// 	return false;
-// }
+template <unsigned int float_prec, unsigned int double_prec>
+bool has_breakdown(const int N, const number<double_prec> a, const number<double_prec> p0, const number<double_prec> thickening_constant, const number<double_prec> error_constant){
+	vector<number<float_prec>> orbit = pseudo_orbit<float_prec>(static_cast<number<float_prec>>(a), static_cast<number<float_prec>>(p0), N);
+	fixed_precision_interval<double_prec> x = fixed_precision_interval<double_prec>(static_cast<number<double_prec>>(orbit[N]), static_cast<number<double_prec>>(orbit[N]));
+	for(int i=N-1; i>=0; i--){
+		
+		bool left= (orbit[i] < number<double_prec>(0.5));
+		x = prev_interval(x, a, thickening_constant, error_constant, left);
+		if(i % 10000 == 0){ 
+			cout<<i<<endl;
+			cout<<x.right<<" "<<a/4<<endl;
+		}
+		if(x.right > a/4)
+			return true;
+	}
+	return false;
+}
 
 // finds the first breakdown point if there is
 // one between l and r, otherwise returns 0
@@ -137,13 +150,49 @@ number<double_prec> shadowing_bound(const int N, const number<double_prec> a, co
 
 int main (int argc,char **argv)
 {
+	namespace mpl = boost::mpl;
 	//cout << binary_search_breakdown(1,100000000)<<endl;
 	const cray_double thickening_constant = pow(cray_double(2.0), -90);
 	const cray_double error_constant = pow(cray_double(10.0), -25);
-	auto x = shadowing_bound<cray_float_precision, cray_double_precision>(10000000, cray_double(3.8), cray_double(0.4), thickening_constant, error_constant);
-	auto x2 = shadowing_bound<10,96>(10000000, number<96>(3.6), number<96>(0.4), pow(number<96>(2.0), -15), pow(number<96>(10.0), -4));
-
-	std::cout << x << " "<<x2<<"\n";
+	//cout << has_breakdown<35, cray_double_precision>(10000000, cray_double(3.8), cray_double(0.4), thickening_constant, error_constant)<<endl;
+	vector<cray_double> as = {3.6, 3.635, 3.65, 3.7, 3.75, 3.8, 3.86, 3.91};
+	vector<cray_double> p0s = {0.3, 0.4, 0.5,0.6};
+	for(auto a : as){
+		for(auto p0 : p0s){
+			auto x = shadowing_bound<5, cray_double_precision>(10000000, a, p0, thickening_constant, error_constant);
+			std::cout << 5 << " " << a << " " << p0 << " " << x << endl;
+			x = shadowing_bound<10, cray_double_precision>(10000000, a, p0, thickening_constant, error_constant);
+			std::cout << 10 << " " << a << " " << p0 << " " << x << endl;
+			x = shadowing_bound<15, cray_double_precision>(10000000, a, p0, thickening_constant, error_constant);
+			std::cout << 15 << " " << a << " " << p0 << " " << x << endl;
+			x = shadowing_bound<20, cray_double_precision>(10000000, a, p0, thickening_constant, error_constant);
+			std::cout << 20 << " " << a << " " << p0 << " " << x << endl;
+			x = shadowing_bound<25, cray_double_precision>(10000000, a, p0, thickening_constant, error_constant);
+			std::cout << 25 << " " << a << " " << p0 << " " << x << endl;
+			x = shadowing_bound<30, cray_double_precision>(10000000, a, p0, thickening_constant, error_constant);
+			std::cout << 30 << " " << a << " " << p0 << " " << x << endl;
+			x = shadowing_bound<35, cray_double_precision>(10000000, a, p0, thickening_constant, error_constant);
+			std::cout << 35 << " " << a << " " << p0 << " " << x << endl;
+			x = shadowing_bound<40, cray_double_precision>(10000000, a, p0, thickening_constant, error_constant);
+			std::cout << 40 << " " << a << " " << p0 << " " << x << endl;
+			x = shadowing_bound<45, cray_double_precision>(10000000, a, p0, thickening_constant, error_constant);
+			std::cout << 45 << " " << a << " " << p0 << " " << x << endl;
+			x = shadowing_bound<50, cray_double_precision>(10000000, a, p0, thickening_constant, error_constant);
+			std::cout << 50 << " " << a << " " << p0 << " " << x << endl;
+			x = shadowing_bound<55, cray_double_precision>(10000000, a, p0, thickening_constant, error_constant);
+			std::cout << 55 << " " << a << " " << p0 << " " << x << endl;
+			x = shadowing_bound<60, cray_double_precision>(10000000, a, p0, thickening_constant, error_constant);
+			std::cout << 60 << " " << a << " " << p0 << " " << x << endl;
+			x = shadowing_bound<65, cray_double_precision>(10000000, a, p0, thickening_constant, error_constant);
+			std::cout << 65 << " " << a << " " << p0 << " " << x << endl;
+			x = shadowing_bound<70, cray_double_precision>(10000000, a, p0, thickening_constant, error_constant);
+			std::cout << 70 << " " << a << " " << p0 << " " << x << endl;
+			x = shadowing_bound<75, cray_double_precision>(10000000, a, p0, thickening_constant, error_constant);
+			std::cout << 75 << " " << a << " " << p0 << " " << x << endl;
+			x = shadowing_bound<80, cray_double_precision>(10000000, a, p0, thickening_constant, error_constant);
+			std::cout << 80 << " " << a << " " << p0 << " " << x << endl;			
+		}
+	}
 
 }
 
